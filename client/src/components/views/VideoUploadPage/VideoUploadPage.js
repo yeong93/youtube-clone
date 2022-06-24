134bc5e38107760727ef455c361bbb7ae5ab4317
sookyeong <sasa023@naver.com>
2022-06-24 15:53:00 +0900

    1. video upload multer add.

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Typography, Button, Form, message, Input, Icon}from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
 const {TextArea} = Input;
 const {Title} = Typography;
@@ -41,6 +42,23 @@ function VideoUploadPage() {
     setCategory(e.currentTarget.value);
   }
 
+  const onDrop = (files) => {
+    let formData = new FormData;
+    const config={
+        header : {'content-type' : 'multipart/form-data'}
+    }
+    formData.append("file",files[0])
+
+    Axios.post('/api/video/uploadfiles', formData, config)
+      .then(response => {
+        if(response.data.success){
+            console.log(response.data);
+        }else{
+            alert("비디오 업로드를 실패했습니다.")
+        }
+      })
+  }
+
   return (
     <div style={{maxWidth:'700px', margin:'2rem auto'}}>
         <div style={{textAlign:'center', marginBottom: '2rem'}}>
@@ -51,7 +69,7 @@ function VideoUploadPage() {
             <div style={{display:'flex', justifyContent:'space-between'}}>
                 {/* Drop zone */}
                 <Dropzone
-                    onDrop
+                    onDrop={onDrop}
                     multiple={false}
                     maxSize={800000000}>
                     {({ getRootProps, getInputProps }) => (
