347fc401e49fd4e00a2c7aa60d55298a461663ac
sookyeong <sasa023@naver.com>
2022-06-30 14:12:56 +0900

    1. reply save function add.

---
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -0,0 +1,66 @@
+import Axios from 'axios';
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+
+function Comment(props) {
+  
+  // url에서 가져온 videoId
+  //const videoId = props.match.params.videoId;
+
+  // props으로 넘긴 videoId
+  const videoId = props.videoId;
+
+  const user = useSelector(state => state.user);
+  const [commentValue, setCommentValue] = useState("");
+
+  const handleClick = (e) => {
+    setCommentValue(e.currentTarget.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variables = {
+        content: commentValue,
+        writer: user.userData._id,
+        postId: videoId
+    }
+
+    Axios.post('/api/comment/saveCommnet', onSubmit)
+      .then(response => {
+        if(response.data.success){
+            console.log(response.data.result);
+        } else {
+            alert('코멘트를 저장하지 못했습니다.');
+        }
+      })
+  }
+
+  return (
+    <div>
+        <br/>
+        <p> Repies </p>
+        <hr />
+
+        {/* Comment Lists */}
+
+
+        {/* Root Comment From */}
+
+
+        <form style={{display: 'flex'}} onSubmit={onSubmit}>
+            <textarea
+                style={{width: '100%', borderRadius: '5px'}}
+                onChange={handleClick}
+                value={commentValue}
+                placeholder='코멘트를 작성해주세요'
+            />
+            <br />
+            <button style={{width: '20%' , height:'52px'}} onClick={onSubmit}>Submit</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default Comment
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import {Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
+import Comment from './Sections/Comment';
 
 function VideoDetailPage(props) {
 
@@ -27,6 +28,10 @@ function VideoDetailPage(props) {
     },[])
   
     if (VideoDetail.writer) {
+
+        // 본인의 글인 경우 subscribe button unvisible
+        const subscribeButton =  VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />
+
         return (
           <Row gutter={[16,16]}>
             <Col lg={18} xs={24}>
@@ -41,7 +46,7 @@ function VideoDetailPage(props) {
                 ></video>
     
                 <List.Item
-                  actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />]}
+                  actions={[subscribeButton]}
                 >
                   <List.Item.Meta
                     avatar={<Avatar src={VideoDetail.writer.image} />}
@@ -51,6 +56,8 @@ function VideoDetailPage(props) {
                   {/* <div></div> */}
                 </List.Item>
                 {/* comments */}
+                <Comment postId={videoId} />
+
               </div>
             </Col>
 
