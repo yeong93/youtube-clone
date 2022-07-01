211c1dd924a26ee688e43c81c9acce123d1faae1
sookyeong <sasa023@naver.com>
2022-07-01 13:42:02 +0900

    1. reply function add.

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -13,7 +13,7 @@ router.post('/saveCommnet', (req, res) => {
     comment.save((err, comment)=>{
         if(err) return res.json({success:false, err})
         
-        Comment.find({'_id' : comment._id})
+        Comment.find({ _id: comment._id })
           .populate('writer')
           .exec((err, result) => {
             if(err) return res.json({success:false, err})
@@ -22,7 +22,18 @@ router.post('/saveCommnet', (req, res) => {
     })
     
     
-})
+});
+
+router.post("/getComments", (req, res) => {
+
+    Comment.find({ "postId": req.body.videoId })
+        .populate('writer')
+        .exec((err, comments) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, comments })
+        })
+
+});
 
 
 module.exports = router;
